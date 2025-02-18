40ff963f7bc8548e60fd60996ae27fc920299123
manhph51702 <manhttph51702@gmail.com>
2025-02-18 15:20:23 +0700

    Create Jhin.py

---
+++ champions/Jhin.py
@@ -0,0 +1,13 @@
+from champion_base import Champion
+class Jhin(Champion): # Jhin kế thừa từ Champion
+    def __init__(self):
+        super().__init__("Jhin", "Sân khấu tử thần")
+
+    def cast_skill_q(self):
+        pass
+    def cast_skill_w(self):
+        pass
+    def cast_skill_r(self):
+        pass
+    def cast_skill_e(self):
+        pass
