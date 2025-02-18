4d1b76947613194024ee0ce8c56f045cd72c6c61
manhph51702 <manhttph51702@gmail.com>
2025-02-18 15:49:43 +0700

    Update Jhin.py

--- champions/Jhin.py
+++ champions/Jhin.py
@@ -2,3 +2,15 @@ from champion_base import Champion
 class Jhin(Champion): # Jhin kế thừa từ Champion
     def __init__(self):
         super().__init__("Jhin", "Dark Matter")
+    def cast_skill_q(self):
+        q = 'Lời thì thầm'
+        return q
+    def cast_skill_w(self):
+        w = 'Lựu đạn nhảy múa'
+        return w
+    def cast_skill_e(self):
+        e = 'Nét vẽ chết chóc'
+        return e
+    def cast_skill_r(self):
+        r = 'Cạm bẫy nghệ thuật'
+        return r
