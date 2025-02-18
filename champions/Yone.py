bac9f3dd5e790fb6caf2bf6262882b1831b3ba40
toilasam <163370211+toilasam@users.noreply.github.com>
2025-02-18 15:49:34 +0700

    Update Yone.py

--- champions/Yone.py
+++ champions/Yone.py
@@ -2,3 +2,16 @@ from champion_base import Champion
 class Zeus(Champion): # Zeus kế thừa từ Champion
     def __init__(self):
         super().__init__("Yone", "Đao kiếm tử thần")
+
+    def cast_skill_q(self):
+        q = 'switcheroo'
+        return q
+    def cast_skill_w(self):
+        w = 'ZAP !'
+        return w
+    def cast_skill_e(self):
+        e = 'flame chompers !'
+        return e
+    def cast_skill_r(self):
+        r = 'super mega death'
+        return r
