b9d34380b6c0287546f0b77be7a1c4c8440c68b0
toilasam <163370211+toilasam@users.noreply.github.com>
2025-02-18 15:53:29 +0700

    Update Yone.py

--- champions/Yone.py
+++ champions/Yone.py
@@ -2,3 +2,16 @@ from champion_base import Champion
 class Yone(Champion):
     def __init__(self):
         super().__init__("Yone", "Dao kiem sanh tu")
+
+    def cast_skill_q(self):
+        q = 'switcheroo'
+        return q
+    def cast_skill_w(self):
+        w = 'ZAP !'
+        return w
+    def cast_skill_e(self):
+        e = 'flame chompers !'
+        return e
+    def cast_skill_r(self):
+        r = 'super mega death'
+        return r
