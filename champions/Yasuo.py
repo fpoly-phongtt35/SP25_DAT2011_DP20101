f51e5c7fb8a03334640f3adbe202746276389d2b
TungTV7325 <yeubanthan252@gmail.com>
2025-02-18 15:56:18 +0700

    Update Yasuo.py

--- champions/Yasuo.py
+++ champions/Yasuo.py
@@ -2,3 +2,15 @@ from champion_base import Champion
 class Yasuo(Champion): # Void kế thừa từ Champion
     def __init__(self):
         super().__init__("Yasuo", "Say Bye")
+
+    def cast_skill_q(self):
+        Q = 'Stell Tempest'
+        return Q
+    def cast_skill_w(self):
+        W = 'Wind Wall'
+        return W
+    def cast_skill_r(self):
+        return self.skill
+    def cast_skill_e(self):
+        E = 'Sweeping Blade'
+        return E
